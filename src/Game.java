4166ea11f4e0bc0e74a7f14ebb242e8e1f4c7530
Korey Heiser <macandrock@gmail.com>
2019-03-06 14:43:05 -0500

    html can now be output to console

--- src/Game.java
+++ src/Game.java
@@ -3,25 +3,37 @@ import java.util.ArrayList;
 public class Game {
     private String title;
     private double cost;
-    private ArrayList<Prize> Prizes;
+    private ArrayList<Prize> prizes;
 
 
     public Game(String title, double cost, ArrayList<Prize> Prizes) {
         this.title = title;
         this.cost = cost;
-        this.Prizes = Prizes;
+        this.prizes = Prizes;
 
     }
+
+   public Game(String title, double cost) {
+        this.title = title;
+        this.cost = cost;
+        prizes = new ArrayList<>();
+   }
 
     public String getTitle() {
         return title;
     }
+
 
     public double getCost() {
         return cost;
     }
 
     public ArrayList<Prize> getPrizes() {
-        return Prizes;
+        return prizes;
     }
+
+    public void addPrize(Prize prize) {
+        prizes.add(prize);
+    }
+
 }
--- src/parseWebsite.java
+++ src/parseWebsite.java
@@ -3,6 +3,7 @@
  **/
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,23 +13,37 @@ import org.jsoup.select.Elements;
 
 public class parseWebsite {
 
+    private ArrayList<Game> games;
+
 
     public static void getWebsite() {
         Document document = null;
         try {
             document = Jsoup.connect("https://www.nclottery.com/ScratchOffPrizes").get();
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if (document == null) {
-            throw new IOException;
+
+        Elements tables = document.select("table");
+
+
+        Elements rows = tables.select("tr");
+
+        for (Element table : tables) {
+            //new table means a new game
+        Elements currentRows = table.select("tr");
+        System.out.print(currentRows.text());
+          //  Game game = new Game();
+
+            for (Element row : rows) {
+                //each row represents a prize
+
+                System.out.print(table.text());
+                System.out.print(row.text() + '\n');
+
+            }
+
         }
-        for(Element table : document.select("table datatable")) {
-            System.out.println(table.text());
-        }
-        //box cloudfx databox price_*
     }
 
 
