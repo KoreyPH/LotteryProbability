c9ad2d6ce2b91f9b37f8b4d3ced3d19d03749f0e
Korey Heiser <macandrock@gmail.com>
2019-03-12 00:46:13 -0400

    method to determine the best odds Game

--- src/LotteryProbability.java
+++ src/LotteryProbability.java
@@ -10,11 +10,33 @@ import java.util.ArrayList;
 public class LotteryProbability {
 
 
+    public static int gameWithBestExpectedValue(ArrayList<Game> games) {
+        int bestGameIndex = 0;
+        for (int i = 0; i < games.size(); i++) {
+
+            double bestMargin = Double.MIN_VALUE;
+
+
+            Game currentGame = games.get(i);
+            double currentExpectedValue = currentGame.calculateExpectedValue();
+
+            if (currentExpectedValue > bestMargin) {
+                bestMargin = currentExpectedValue;
+                bestGameIndex = i;
+            }
+
+        }
+
+        return bestGameIndex;
+    }
+
+
     public static void main(String[] args) {
 
         long startTime = System.nanoTime();
 
         ArrayList<Game> gamesList = parseWebsite.scrapeWebsite();
+
 
         System.out.print("Displaying the ticket with the overall best odds: \n");
 
@@ -35,7 +57,8 @@ public class LotteryProbability {
         System.out.print("The current best odds game is " + winner.getTitle() + " with odds of winning being 1 in " + lowestOdds + ".\n");
         System.out.print("The cost of this ticket is " + winner.getCost() + " dollars\n");
 
-        System.out.print("The expected value of " + gamesList.get(2).getTitle() + " is " + gamesList.get(2).calculateExpectedValue());
+        int bestMarginGame = gameWithBestExpectedValue(gamesList);
+        System.out.print("The game with the best expected value is " + gamesList.get(bestMarginGame).getTitle() + " with E(x) =  " + gamesList.get(bestMarginGame).calculateExpectedValue());
 
         System.out.print("\nCompleted in " + totalTime + " seconds");
 
